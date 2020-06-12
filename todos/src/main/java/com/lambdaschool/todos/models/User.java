513fc8874d5c75baa9f19d9cf20f161504fb7430
Ian McElroy <mcelroyian@gmail.com>
2020-06-12 11:02:11 -0500

    init

---
+++ todos/src/main/java/com/lambdaschool/todos/models/User.java
@@ -0,0 +1,83 @@
+package com.lambdaschool.todos.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String primaryemail;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true)
+    @JsonIgnoreProperties(value = "user",
+        allowSetters = true)
+    private List<Todos> todos = new ArrayList<>();
+
+
+    public User() {
+    }
+
+    public User(String username, String primaryemail, String password, List<Todos> todos) {
+        setUsername(username);
+        this.primaryemail = primaryemail;
+        this.password = password;
+        this.todos = todos;
+    }
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(long userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username.toLowerCase();
+    }
+
+    public void setUsername(String username) {
+        this.username = username.toLowerCase();
+    }
+
+    public String getPrimaryemail() {
+        return primaryemail;
+    }
+
+    public void setPrimaryemail(String primaryemail) {
+        this.primaryemail = primaryemail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Todos> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<Todos> todos) {
+        this.todos = todos;
+    }
+}
